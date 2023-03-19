9e92fa37e60ee9446d5eb90db41212d992aabc51
STGorbunovDA <bous07@mail.ru>
2023-03-19 17:13:37 +0800

    Без применения ORM

--- ORMPostgreSQL/App.cs
+++ ORMPostgreSQL/App.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ORMPostgreSQL
+﻿namespace ORMPostgreSQL
 {
     internal class App<T>
         where T : Worker
--- ORMPostgreSQL/Program.cs
+++ ORMPostgreSQL/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// Создаём новое приложение с Worker и в качестве хранилища данных
+// использую ListRepository
+using ORMPostgreSQL;
+
+App<Worker> app = new App<Worker>(new ListRepository());
+
+app.Create(new Worker("Василий Иванов", 22, 3312));
+app.Create(new Worker("Василий Петров", 44, 3312));
+app.Create(new Worker("Иван Васильев", 33, 3312));
+
+app.Print(); Console.WriteLine();
+
+app.Remove(2);
+app.Print(); Console.WriteLine();
+
+app.Update(1, new Worker("Sergei Kim", 122, 31312));
+app.Print(); Console.WriteLine();
+Console.ReadLine();
