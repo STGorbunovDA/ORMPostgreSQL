98a650c3a1b67b3c3cc46c235e9121647b946de4
STGorbunovDA <bous07@mail.ru>
2023-03-19 17:06:34 +0800

    Добавление модели

---
+++ ORMPostgreSQL/Worker.cs
@@ -0,0 +1,21 @@
+﻿namespace ORMPostgreSQL
+{
+    internal class Worker
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+        public string? FullName { get; set; }
+
+        public Worker() { }
+        public Worker(string fullName, int age, int salary)
+        {
+            Age = age;
+            Salary = salary;
+            FullName = fullName;
+        }
+
+        public override string ToString() => $"[{Id} {FullName} {Age} {Salary}]";
+
+    }
+}
