b1f4be40ed183293eb7a23b4926b9704aeeb6934
STGorbunovDA <bous07@mail.ru>
2023-03-19 17:12:34 +0800

    Создаём класс позволяющий работать с разными представлениями(репозиториями) для разных моделей

---
+++ ORMPostgreSQL/App.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORMPostgreSQL
+{
+    internal class App<T>
+        where T : Worker
+    {
+        IRepository<T> current;
+        public App(IRepository<T> repo) => current = repo; // создание кон-го репозитория с которым пороводим работу
+        public void Create(T item) => current.Create(item);
+        public void Update(int id, T modified) =>
+            current.Update(id, modified);
+        public void Remove(int id) => current.Delete(id);
+        public void Print() =>
+            Console.WriteLine(String.Join<T>("\n", current.Read()));
+
+    }
+}
