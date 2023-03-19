68295806d7681d0684d3022e864cec62951da4b8
STGorbunovDA <bous07@mail.ru>
2023-03-19 17:32:01 +0800

    Создаём подключение к БД

---
+++ ORMPostgreSQL/StorageContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ORMPostgreSQL
+{
+    internal class StorageContext : DbContext
+    {
+        string dbName;
+        public StorageContext (string dbName)
+        {
+            this.dbName = dbName;
+        }
+        public DbSet<Worker> WorkersTable { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString =
+                @"Host{0}; Username = {1}; Password = {2}; Database = {3}";
+            optionsBuilder.UseNpgsql(
+                String.Format(connectionString, 
+                "localhost:5432",
+                "postgres",
+                "w2mir14",
+                dbName
+                ));
+
+        }
+    }
+}
