bdb5dcae9e61c85440e07d316e8b0594c9f29f2b
STGorbunovDA <bous07@mail.ru>
2023-03-19 17:09:00 +0800

    Создаем интерфейс (абстрактный репозиторий) для реализации методов по работе с моделью

---
+++ ORMPostgreSQL/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORMPostgreSQL
+{
+    internal interface IRepository<T>
+    {
+        void Create(T item);
+        T[] Read();
+        void Update(int id, T newItem);
+        void Delete(int Id);
+    }
+}
